f25f5d84251749fe31766aef74dd24ac4399efd8
ShiningAsStar <gipnossasha2000@gmail.com>
2026-06-09 13:56:10 +0200

    Update shining.sh

--- tools/shining.sh
+++ tools/shining.sh
@@ -1,152 +1,64 @@
-getvalue() { grep "^$1=" "$2" 2>/dev/null | tail -n1 | cut -d= -f2-; }
-
-is_sideload() {
-  if getprop sys.usb.state | grep -q "sideload"; then
-    return 0
-  fi
-
-  return 1
-}
-
-keycheck() {
+# Kernel selection function
+choose_kernel() {
   ui_print " "
-  ui_print "Checking volume keys..."
-  ui_print "  Press (VOL +) or (VOL -) to confirm!"
-  ui_print "  Waiting 10 seconds..."
-
-  local timeout=10
-  local start=$(date +%s)
+  ui_print "Kernel Version Selection:"
+  ui_print " "
+  ui_print "  VOL + : Vanilla variant"
+  ui_print "  VOL - : KSU variant"
+  ui_print " "
+  ui_print "Waiting for input... "
+  ui_print " "
+  ui_print " "
 
   while true; do
-    case $(timeout 0.2 getevent -qlc 1 2>/dev/null | grep -m1 "KEY_VOLUME") in
-      *KEY_VOLUMEUP*|*KEY_VOLUMEDOWN*)
-        return 0
-        break;;
-    esac
-
-    local cur=$(date +%s)
-    if [[ $(($cur - $start)) -ge $timeout ]]; then
-      return 1
-    fi
-  done
-}
-
-# vol_selectopt <message> <1st opt> <2nd opt>
-vol_selectopt() {
-  sleep 0.5
-  ui_print " "
-  ui_print "$1"
-  ui_print " - (VOL +) $2"
-  ui_print " - (VOL -) $3"
-  while true; do
-    case $(getevent -lt 2>/dev/null | grep -m1 "KEY_VOLUME") in
+    input=$(getevent -qlc 1 2>/dev/null | grep -E "KEY_VOLUME(UP|DOWN)")
+    case "$input" in
       *KEY_VOLUMEUP*)
-        local selected=$2
-        ui_print "$2 Selected!"
-        break;;
+        return 1
+        ;;
       *KEY_VOLUMEDOWN*)
-        local selected=$3
-        ui_print "$3 Selected!"
-        break;;
+        return 2
+        ;;
     esac
+    sleep 0.1
   done
-  ui_print " "
-  echo "$selected"
 }
 
-### stuff ###
-
-kernvar="default"
-dtbo="normal"
-
-var_select() {
-  local opt=$(vol_selectopt "Select Variant" "Default (NONROOT)" "KernelSU")
-  case "$opt" in
-    *Default*|*NONROOT*)
-      kernvar="default";;
-    *KernelSU*)
-      kernvar="ksu";;
+# Handle kernel selection
+if [ -f "$AKHOME/Image.ksu" ] && [ -f "$AKHOME/Image.noksu" ]; then
+  choose_kernel
+  case $? in
+    1)
+      ui_print " "
+      ui_print "Selected: Vanilla variant Kernel"
+      mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
+      ;;
+    2)
+      ui_print " "
+      ui_print "Selected: KSU variant Kernel"
+      mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+      ;;
   esac
-}
-
-dtbo_select_alioth() {
-  opt=$(vol_selectopt "DTBO Type" "Normal" "5K mAh")
-  case "$opt" in
-    *Normal*)
-      dtbo="normal";;
-    *5K*)
-      dtbo="5k";;
-  esac
-}
-
-autoinstall() {
-  if [[ -f kernel.conf ]]; then
-    ui_print "Found kernel.conf inside zip!"
-    ui_print "Loading configuration from file..."
-    
-    local variant=$(getvalue variant kernel.conf)
-    if [ ! -z "$variant" ]; then
-      kernvar="$variant"
-      if [[ $kernvar == "ksu" ]]; then
-        ui_print "- Using KernelSU Variant"
-      else
-        ui_print "- Using NONROOT Variant"
-      fi
-    fi
-    
-    local dtbo_type=$(getvalue dtbo kernel.conf)
-    if [ ! -z "$dtbo_type" ]; then
-      dtbo="$dtbo_type"
-      if [[ $dtbo == "5k" ]] || [[ $dtbo == "5K" ]] || [[ $dtbo == "5000" ]]; then
-        ui_print "- Using 5k mAh DTBO"
-        dtbo="5k"
-      else
-        ui_print "- Normal DTBO"
-        dtbo="normal"
-      fi
-    fi
-  else
-    ui_print "kernel.conf is not found! aborting autoinstall..."
-    selectorinstall
-  fi
-}
-
-selectorinstall() {
-  if keycheck; then
-    var_select
-    if [[ -f dtbo-5k.img ]]; then
-      dtbo_select_alioth
-    fi
-  else
-    ui_print " "
-    ui_print "Timed out! using autoinstall..."
-    ui_print " "
-    autoinstall
-  fi
-}
-
-startinstall() {
-  if is_sideload; then
-    ui_print "Sideload install detected!"
-    autoinstall
-  else
-    if [[ "$ZIPFILE" == *auto* ]]; then
-      autoinstall
-    else
-      selectorinstall
-    fi
-  fi
-}
-
-applyconfig() {
+elif [ -f "$AKHOME/Image" ]; then
   ui_print " "
-  ui_print "Applying configuration..."
-  if [[ $kernvar == "ksu" ]]; then
-    mv -f Image-ksu.gz Image.gz
-  fi
-  if [[ -f dtbo-5k.img ]]; then
-    if [[ $dtbo == "5k" ]]; then
-      mv -f dtbo-5k.img dtbo.img;
-    fi
-  fi
-}
+  ui_print "Single image kernel found, flashing it"
+  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+elif [ -f "$AKHOME/Image.ksu" ]; then
+  ui_print " "
+  ui_print "Only KernelSU variant version found, flashing it"
+  mv -f "$AKHOME/Image.ksu" "$AKHOME/Image"
+elif [ -f "$AKHOME/Image.noksu" ]; then
+  ui_print " "
+  ui_print "Only Vanilla variant version found, flashing it"
+  mv -f "$AKHOME/Image.noksu" "$AKHOME/Image"
+fi
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
