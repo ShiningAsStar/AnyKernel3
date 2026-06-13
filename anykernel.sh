b268bae2367f8344f9f09c8aa1c63682f5d26dc1
ShiningAsStar <gipnossasha2000@gmail.com>
2026-06-13 17:13:37 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Shining By Shiney
+kernel.string=ShiningKernel by @ShineyStarr @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,8 +12,8 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=sapphire
 device.name2=sapphiren
-device.name3=
-device.name4=
+device.name3=tapas
+device.name4=topaz
 device.name5=
 supported.versions=
 supported.patchlevels=
